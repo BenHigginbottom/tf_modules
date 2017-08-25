abbafe5c299d69a2c2c9510eca6084ed6cc40476
BenHigginbottom <ben.higginbottom@gmail.com>
2017-08-25 16:01:10 +0100

    Initial module, playing with gathering info

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region                  = "${var.aws_region}"
+  shared_credentials_file = "/home/ben/.aws/credentials"
+}
+
+module "caller" {
+	source = "./modules"
+}
+
+
+output "account_id" {
+  value = "${module.caller.account_id}"
+}
+
+output "caller_arn" {
+  value = "${module.caller.caller_arn}"
+}
+
+output "caller_user" {
+  value = "${module.caller.caller_user}"
+}
+
+
