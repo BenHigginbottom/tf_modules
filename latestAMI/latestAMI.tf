f6be3460e14a2adad8b9bddb46cb7443a5655314
BenH <ben.higginbottom@gmail.com>
2017-08-29 16:01:16 +0100

    Finally passes the plan, likely a reserved word - check!

---
+++ latestAMI/latestAMI.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ec2-linux" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["amzn-ami-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+
+
+output "ec2linuxd" {
+  value = "${data.aws_ami.ec2-linux.image_id}"
+}
+
+
--- main.tf
+++ main.tf
@@ -22,14 +22,16 @@ module "latestAMI" {
   source = "./latestAMI"
 }
 
-/*
 module "ec2dist" {
   source = "./ec2dist"
   INSTANCES = 3
   AZ = "${module.scan.names}"
-  AMI = "${module.latestAMI.amiid}"
+  AMI = "${module.latestAMI.ec2linuxd}"
 }
-*/
+
+
+/*Debug Outputs*/
+
 output "account_id" {
   value = "${module.caller.account_id}"
 }
@@ -58,6 +60,6 @@ output "SG" {
   value = "${module.scan.security_group}"
 }
 
-output "INFO" {
-  value = "${module.latestAMI.amiidlatest}"
+output "info" {
+  value = "${module.latestAMI.ec2linuxd}"
 }
