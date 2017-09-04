44b7eeabb467c0ec6eaf608dad26f0380c1b86a6
BenH <ben.higginbottom@gmail.com>
2017-09-04 16:23:59 +0100

    Updating scan output

---
+++ scan/outputs.tf
@@ -0,0 +1,24 @@
+output "names" {
+  value = "${data.aws_availability_zones.available.names}"
+}
+
+output "vpcstuff" {
+  value = "${data.aws_vpc.default.id}"
+}
+
+output "subnets" {
+  value = "${data.aws_subnet_ids.Testing.ids}"
+}
+
+output "security_group" {
+  value = "${data.aws_security_group.Testing.id}"
+}
+
+output "dbsnet" {
+  value = "${data.aws_subnet.databasesnet.id}"
+}
+
+
+output "rdsenckey" {
+  value = "${data.aws_kms_alias.rds.arn}"
+}
