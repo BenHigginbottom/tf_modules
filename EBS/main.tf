0f3049bde15d8de0e33e537f69b87c499fd916bb
BenHigginbottom <ben.higginbottom@gmail.com>
2017-09-08 12:17:35 +0100

    Made EBS AZ a loop over a list

--- EBS/main.tf
+++ EBS/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "example" {
   count = "${var.Count}"
-  availability_zone = "${var.AvZ}"
+  availability_zone = "${element(var.AvZ, count.index)}"
   encrypted = "true"
   kms_key_id = "${var.EBSKey}"
   size = "${var.Size}"
